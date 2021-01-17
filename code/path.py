40342f41afa03d1df0efb80c0491a4d56c7c3d1f
Michael Szell <michael.szell@gmail.com>
2021-01-17 20:53:18 +0100

    cities by size
    - also moved exports and logs to external folder
    - removed beihai remnants

--- code/path.py
+++ code/path.py
@@ -5,7 +5,7 @@ PATH["plots"] = "../plots/"
 PATH["plots_networks"] = "../../bikenwgrowth_external/plotsnetworks/"
 PATH["results"] = "../../bikenwgrowth_external/results/"
 PATH["videos"] = "../../bikenwgrowth_external/videos/"
-PATH["exports"] = "../exports/"
-PATH["logs"] = "../logs/"
+PATH["exports"] = "../../bikenwgrowth_external/exports/"
+PATH["logs"] = "../../bikenwgrowth_external/logs/"
 
 print("Loaded PATH")
