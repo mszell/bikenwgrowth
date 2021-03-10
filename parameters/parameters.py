745de80384a22880b956afdcee8a49cced54f760
Michael Szell <michael.szell@gmail.com>
2021-03-10 17:48:47 +0100

    videos: generating mp4 instead of avi

--- parameters/parameters.py
+++ parameters/parameters.py
@@ -1,7 +1,7 @@
 # PARAMETERS
 # These are values to loop through for different runs
 poi_source = "railwaystation" # railwaystation, grid
-prune_measure = "closeness" # betweenness, closeness, random
+prune_measure = "betweenness" # betweenness, closeness, random
 
 SERVER = False # Whether the code runs on the server (important so parallel jobs don't interfere)
 
